09317010b88bb145db9b4c0c1fcdcf6f9bb0e62f
Marc Hanheide <marc@hanheide.net>
2013-11-06 13:09:13 +0000

    restructured to make it mergaeable with the master

--- apache/configure_apache.sh
+++ apache/configure_apache.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+set -x
+
 scitos_desc=`rospack find scitos_description`
 webtools=`rospack find strands_webtools`
 apache_doc_root="/var/www/strands"
 apache_avail_sites="/etc/apache2/sites-available"
 
 sudo install -d $apache_doc_root
-sudo cp -af css/ $apache_doc_root/css
-sudo cp -af js/ $apache_doc_root/js
-find -name "build" -type d | xargs -i sudo cp --parents -af {} $apache_doc_root/
-find -name "include" -type d | xargs -i sudo cp --parents -af {} $apache_doc_root/
+sudo cp -af $webtools/css/ $apache_doc_root/css
+sudo cp -af $webtools/js/ $apache_doc_root/js
+find $webtools -name "build" -type d | xargs -i sudo cp --parents -af {} $apache_doc_root/
+find $webtools -name "include" -type d | xargs -i sudo cp --parents -af {} $apache_doc_root/
 
 sudo install -d $apache_doc_root/scitos_description
 
 sudo cp -af $scitos_desc/meshes $apache_doc_root/scitos_description
 
 
-sudo install -p *.html $apache_doc_root
+sudo install -p $webtools/*.html $apache_doc_root
 
-sudo install -p strands.site $apache_avail_sites/strands
+for s in *.site; do 
+	bn=`basename "$s" .site`
+	sudo install -p ./$s $apache_avail_sites/$bn
+done
 
 # enable apache modules
 sudo a2enmod proxy* rewrite*
